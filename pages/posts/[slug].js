4dc1ad99af27d9a21ed7c071ce33e6a7a554c6e4
Farino Joshua <96770446+farinojoshua@users.noreply.github.com>
2022-06-18 15:55:39 +0700

    second commit

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,4 +1,4 @@
-import styles from "../../styles/Slug.module.css";
+import styles from "../../styles/slug.module.css";
 import { GraphQLClient, gql } from "graphql-request";
 import moment from "moment";
 
